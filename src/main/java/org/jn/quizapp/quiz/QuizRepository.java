55ee76883e2ef4db16aa5141fd0fab83d9d268a5
Jani Nieminen <jpkniem@gmail.com>
2015-08-08 20:47:37 +0300

    Mongo repository.

--- src/main/java/org/jn/quizapp/quiz/QuizRepository.java
+++ src/main/java/org/jn/quizapp/quiz/QuizRepository.java
@@ -1,7 +1,10 @@
 package org.jn.quizapp.quiz;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface QuizRepository extends MongoRepository<Quiz, String> {
-	
+	public List<Quiz> findAll();
+	public Quiz findOne(String id);
 }
