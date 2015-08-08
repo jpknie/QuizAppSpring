25f1955f2751edb33980d6ccdf44ee224c9fc50e
Jani Nieminen <jpkniem@gmail.com>
2015-08-08 20:56:53 +0300

    Seed the data in the application class.

--- src/main/java/org/jn/quizapp/Application.java
+++ src/main/java/org/jn/quizapp/Application.java
@@ -1,17 +1,52 @@
 package org.jn.quizapp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.jn.quizapp.quiz.Quiz;
+import org.jn.quizapp.quiz.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @ComponentScan
-public class Application {
+public class Application implements CommandLineRunner {
 
+	@Autowired
+	QuizRepository quizRepository;
+
+	public void run(String... args) throws Exception {
+		if(quizRepository.count() == 0) {
+			System.out.println("Seeding the database..");
+			Seeder.seed(quizRepository);
+		}
+		else {
+			System.out.println("Data is present in the database.. Skip seeding.");
+		}
+	}
+	
     public static void main(String[] args) {
+    
         SpringApplication.run(Application.class, args);
     }
+    
+    static class Seeder {
+    	public static void seed(QuizRepository quizRepository) {
+    		Quiz quizOne = new Quiz("Miscellaneous", "What is name of my cat?", "cat.jpg", 
+        			Arrays.asList("Bob", "Tom", "Alice"),
+        			0
+        			);
+    		Quiz quizTwo = new Quiz("Miscellaneous", "What's in my pocket?", "pocket.jpg",
+    				Arrays.asList("Candy", "Your Hand", "My Hand"),
+    				2
+    				);
+        	quizRepository.save(quizOne);
+        	quizRepository.save(quizTwo);
+    	}
+    }
+    
 }
